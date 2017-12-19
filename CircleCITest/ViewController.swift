23f05b39193609286aa5acfe578cc427dc0f3afa
michihironozawa <hiro7355@gmail.com>
2017-12-19 15:54:45 +0900

    new

---
+++ CircleCITest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleCITest
+//
+//  Created by 野澤 通弘 on 2017/12/19.
+//  Copyright © 2017年 ikaika software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
